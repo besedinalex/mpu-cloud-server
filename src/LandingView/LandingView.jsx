a01b71e485ca2ddf4102a0fe30d473b51b5881c2
Alexey <besedinalexey@gmail.com>
2019-09-15 08:25:10 +0300

    Doesn't suggest to log if logged in, translation, updated carousel code

--- src/LandingView/LandingView.jsx
+++ src/LandingView/LandingView.jsx
@@ -1,26 +1,54 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
 
-import './LandingView.css';
-
 import HeaderComponent from "../HeaderComponent";
 
+import './LandingView.css';
+
 class LandingView extends Component {
+    constructor(props) {
+        super(props);
+
+        const session = JSON.parse(localStorage.getItem('session'));
+
+        this.state = {
+            isAuthenticated: session !== null && Date.now() <= session.expiresAt
+        };
+    }
+
     render() {
+        const loggedIn = this.state.isAuthenticated;
+        const carouselItems = [
+            {
+                text: 'Храни все модели в одном месте',
+                desc: 'Все ваши разработки будут в одном месте. Вы можете получить к ним доступ в в любое время любом месте прямо в вашем браузере.',
+                img: '',
+                id: 0
+            },
+            {
+                text: 'GLTF',
+                desc: 'Конвертирует ваши модели из множества распространненых форматов (например, из STEP) в GLTF.',
+                img: ''
+            },
+            {
+                text: 'Доступ с любого устройства',
+                desc: 'Вы можете смотреть, вращать и показывать свои работы с любого устройства на котором есть веб браузер.',
+                img: ''
+            }
+        ];
         return (
             <div className="margin-for-header">
                 <HeaderComponent />
                 <div className="position-relative overflow-hidden p-3 p-md-3 text-center bg-light">
                     <div className="col-md-5 p-lg-5 mx-auto my-5">
                         <h1 className="display-4 font-weight-normal">MPU Cloud</h1>
-                        <p className="lead font-weight-normal">Designed to be the best cloud storage for engineers.</p>
+                        <p className="lead font-weight-normal">Лучшее облачное хранилище для инженеров.</p>
                         <div>
-                            <Link className="btn btn-outline-info mx-1" to="/signup">Sign Up</Link>
-                            <Link className="btn btn-outline-dark mx-1" to="/login">Log In</Link>
+                            <Link hidden={loggedIn} className="btn btn-outline-info mx-1" to="/signup">Зарегистрироваться</Link>
+                            <Link hidden={loggedIn} className="btn btn-outline-dark mx-1" to="/login">Войти</Link>
+                            <Link hidden={!loggedIn} className="btn btn-outline-primary" to="/models">Ваши модели</Link>
                         </div>
                     </div>
-                    <div className="product-device shadow-sm d-none d-md-block" />
-                    <div className="product-device product-device-2 shadow-sm d-none d-md-block" />
                 </div>
 
                 <div id="myCarousel" className="carousel slide" data-ride="carousel">
@@ -30,49 +58,24 @@ class LandingView extends Component {
                         <li data-target="#myCarousel" data-slide-to="2" />
                     </ol>
                     <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <svg className="bd-placeholder-img" width="100%" height="100%"
-                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-                                 focusable="false" role="img">
-                                <rect width="100%" height="100%" fill="#777" />
-                            </svg>
-                            <div className="container">
-                                <div className="carousel-caption">
-                                    <h1>Keep your 3D models in one place</h1>
-                                    <p>All your 3D models will be in one place. You can access them anytime anywhere in
-                                        the world just from your browser.</p>
-                                    <p><a className="btn btn-lg btn-primary" href="#" role="button">Placeholder</a></p>
+                        {carouselItems.map(item => {
+                            const itemClass = item.id === 0 ? 'carousel-item active' : 'carousel-item';
+                            return (
+                                <div className={itemClass}>
+                                    <svg className="bd-placeholder-img" width="100%" height="100%"
+                                         xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
+                                         focusable="false" role="img">
+                                        <rect width="100%" height="100%" fill="#777"/>
+                                    </svg>
+                                    <div className="container">
+                                        <div className="carousel-caption">
+                                            <h1>{item.text}</h1>
+                                            <p>{item.desc}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <svg className="bd-placeholder-img" width="100%" height="100%"
-                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-                                 focusable="false" role="img">
-                                <rect width="100%" height="100%" fill="#777"/>
-                            </svg>
-                            <div className="container">
-                                <div className="carousel-caption">
-                                    <h1>From many to GLTF</h1>
-                                    <p>Converts your models from many popular formats (STP, STP, STP) to GLTF.</p>
-                                    <p><a className="btn btn-lg btn-primary" href="#" role="button">Placeholder</a></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <svg className="bd-placeholder-img" width="100%" height="100%"
-                                 xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"
-                                 focusable="false" role="img">
-                                <rect width="100%" height="100%" fill="#777"/>
-                            </svg>
-                            <div className="container">
-                                <div className="carousel-caption">
-                                    <h1>View your model from any device</h1>
-                                    <p>You can view, rotate, show your model from any device with web browser.</p>
-                                    <p><a className="btn btn-lg btn-primary" href="#" role="button">Placeholder</a></p>
-                                </div>
-                            </div>
-                        </div>
+                            );
+                        })}
                     </div>
                     <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true" />
@@ -86,7 +89,7 @@ class LandingView extends Component {
 
                 <div className="position-relative overflow-hidden p-3 p-md-3 text-center bg-light">
                     <div className="col-md-5 p-lg-5 mx-auto my-5">
-                        <h1 className="display-5 font-weight-normal">Start using today!</h1>
+                        <h1 className="display-5 font-weight-normal">Начните пользоваться сегодня!</h1>
                     </div>
                 </div>
             </div>
