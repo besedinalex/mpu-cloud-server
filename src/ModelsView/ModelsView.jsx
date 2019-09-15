e247ff98d46c09a103cda5358e6ba484f0bce084
Alexey <besedinalexey@gmail.com>
2019-09-15 06:59:51 +0300

    Refactoring

--- src/ModelsView/ModelsView.jsx
+++ src/ModelsView/ModelsView.jsx
@@ -13,12 +13,12 @@ class ModelsView extends Component {
 
         this.state = {
             models: [],
-            isDiaglogOpen: false,
+            isDialogOpen: false,
             title: '',
             desc: '',
             filename: '',
             isUploaded: false
-        }
+        };
 
         this.fileInput = React.createRef();
 
@@ -33,36 +33,36 @@ class ModelsView extends Component {
 
     componentDidMount() {
         axios.get(`http://127.0.0.1:4000/models?token=${this.props.token}`).then(res => {
-            console.log(res.data)
-            this.setState({ models: res.data.reverse() })
+            console.log(res.data);
+            this.setState({ models: res.data.reverse() });
         })
     }
 
     handleCloseDialog() {
-        this.setState({ isDiaglogOpen: false })
+        this.setState({ isDialogOpen: false });
     }
 
     handleOpenDialog() {
-        this.setState({ isDiaglogOpen: true })
+        this.setState({ isDialogOpen: true });
     }
 
     handleTitleChange(e) {
-        this.setState({ title: e.target.value })
+        this.setState({ title: e.target.value });
     }
 
     handleDescChange(e) {
-        this.setState({ desc: e.target.value })
+        this.setState({ desc: e.target.value });
     }
 
     handleFileSelection() {
-        console.log(this.fileInput.current.files)
+        console.log(this.fileInput.current.files);
         if (this.fileInput.current.files) this.setState({ filename: this.fileInput.current.files[0].name });
     }
 
     handleModelRemoved(data) {
         let newModels = this.state.models.filter(model => {
             return model.model_id !== data.id;
-        })
+        });
         console.log(newModels)
         this.setState(prev => ({
             models: newModels
@@ -70,15 +70,15 @@ class ModelsView extends Component {
     }
 
     handleUpload() {
-        this.setState({ isUploaded: true })
-        console.log(this.fileInput, this.state)
+        this.setState({ isUploaded: true });
+        console.log(this.fileInput, this.state);
         let bodyFormData = new FormData();
 
-        bodyFormData.append('title', this.state.title)
-        bodyFormData.append('desc', this.state.desc)
+        bodyFormData.append('title', this.state.title);
+        bodyFormData.append('desc', this.state.desc);
         bodyFormData.append('model', this.fileInput.current.files[0]);
 
-        for (var pair of bodyFormData.entries()) {
+        for (const pair of bodyFormData.entries()) {
             console.log(pair[0] + ', ' + pair[1]);
         }
 
@@ -93,9 +93,9 @@ class ModelsView extends Component {
                 console.log(res.data);
 
                 this.setState(prev => ({
-                    isUploaded: false, isDiaglogOpen: false, models: [res.data, ...prev.models]
+                    isUploaded: false, isDialogOpen: false, models: [res.data, ...prev.models]
                 }));
-                $('#exampleModal').modal('hide')
+                $('#exampleModal').modal('hide');
             })
             .catch(function (response) {
                 //handle error
@@ -103,12 +103,11 @@ class ModelsView extends Component {
             });
     }
 
-
     render() {
-        console.log(this.state.isDiaglogOpen)
+        console.log(this.state.isDialogOpen);
 
         const modelCells = this.state.models.map(model => {
-            console.log(model)
+            console.log(model);
             return (
                 <ModelItem
                     id={model.model_id}
@@ -121,46 +120,46 @@ class ModelsView extends Component {
                 >
                 </ModelItem>
             );
-        })
+        });
 
         return (
             <div className="margin-for-header">
-                <div hidden={!this.state.isDiaglogOpen} class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Добавить модель</h5>
-                                <button onClick={this.handleCloseDialog} type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <div hidden={!this.state.isDialogOpen} className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Добавить модель</h5>
+                                <button onClick={this.handleCloseDialog} type="button" className="close" data-dismiss="modal" aria-label="Close">
                                     <span>&times;</span>
                                 </button>
                             </div>
-                            <div class="modal-body">
-                                <div class="input-group" style={{ marginBottom: '16px' }}>
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text" id="">Название</span>
+                            <div className="modal-body">
+                                <div className="input-group" style={{ marginBottom: '16px' }}>
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="">Название</span>
                                     </div>
-                                    <input name="author" type="text" class="form-control" onChange={this.handleTitleChange} required />
+                                    <input name="author" type="text" className="form-control" onChange={this.handleTitleChange} required />
                                 </div>
-                                <div class="form-group" style={{ textAlign: 'left' }} >
-                                    <label style={{ textAlign: 'left' }} for="exampleFormControlTextarea1">Описание</label>
-                                    <textarea name="desc" class="form-control" id="exampleFormControlTextarea1" rows="5" onChange={this.handleDescChange} required></textarea>
+                                <div className="form-group" style={{ textAlign: 'left' }} >
+                                    <label style={{ textAlign: 'left' }} htmlFor="exampleFormControlTextarea1">Описание</label>
+                                    <textarea name="desc" className="form-control" id="exampleFormControlTextarea1" rows="5" onChange={this.handleDescChange} required />
                                 </div>
-                                <div class="custom-file">
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input ref={this.fileInput} class="custom-file-input" name="model" id="inputGroupFile04" type="file" required="" />
-                                            <label style={{ textAlign: 'left' }} class="custom-file-label" for="inputGroupFile04">Выберете файл</label>
+                                <div className="custom-file">
+                                    <div className="input-group">
+                                        <div className="custom-file">
+                                            <input ref={this.fileInput} className="custom-file-input" name="model" id="inputGroupFile04" type="file" required="" />
+                                            <label style={{ textAlign: 'left' }} className="custom-file-label" htmlFor="inputGroupFile04">Выберете файл</label>
                                         </div>
 
                                     </div>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button onClick={this.handleCloseDialog} type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
-                                {this.state.isUploaded ? <button class="btn btn-primary" type="button" disabled>
-                                    <span style={{ marginRight: '8px' }} class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                            <div className="modal-footer">
+                                <button onClick={this.handleCloseDialog} type="button" className="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                                {this.state.isUploaded ? <button className="btn btn-primary" type="button" disabled>
+                                    <span style={{ marginRight: '8px' }} className="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
                                     Загрузка...
-</button> : <button type="button" class="btn btn-primary" onClick={this.handleUpload}>Отправить</button>}
+</button> : <button type="button" className="btn btn-primary" onClick={this.handleUpload}>Отправить</button>}
 
                             </div>
                         </div>
@@ -173,7 +172,7 @@ class ModelsView extends Component {
                 <div className="container">
                     <div style={{ marginBottom: 20 + 'px', display: 'flex', justifyContent: 'space-between' }}>
                         <h3>Мои модели</h3>
-                        <button style={{ height: '42px' }} onClick={this.handleOpenDialog} type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        <button style={{ height: '42px' }} onClick={this.handleOpenDialog} type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                             Добавить
 </button>
                     </div>
@@ -181,7 +180,7 @@ class ModelsView extends Component {
                         <thead>
                             <tr>
                                 <th scope="col">Название</th>
-                                <th scope="col"></th>
+                                <th scope="col" />
                                 <th scope="col">Тип</th>
                                 <th scope="col">Вес</th>
                                 <th scope="col">Загружено</th>
