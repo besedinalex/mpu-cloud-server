9940d1f7227286ff0662deb4bce01e5489104524
Sevka <sevkaplay@gmail.com>
2019-09-22 15:35:38 +0300

    Updated ModelsView group&models mod. Added UserView

--- src/components/Groups/UsersView/UsersView.jsx
+++ src/components/Groups/UsersView/UsersView.jsx
@@ -1,105 +1,40 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import axios from "axios";
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUserFriends, faFile} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserFriends, faFile } from "@fortawesome/free-solid-svg-icons";
+
+import UserView from "../UserComponent";
+import { getGroupUsers } from "../../services/users";
 
 class UsersView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount = () => this.getGroupUsers();
 
-    render() {
-        return (
-            <div>
-                <main role="main" class="container">
-                    <div class="my-3 p-3 bg-white rounded shadow-sm">
-                        <h6 class="border-bottom border-gray pb-2 mb-0">Suggestions</h6>
-                        <div class="media text-muted pt-3">
-                            <svg
-                                class="bd-placeholder-img mr-2 rounded"
-                                width="32"
-                                height="32"
-                                xmlns="http://www.w3.org/2000/svg"
-                                preserveAspectRatio="xMidYMid slice"
-                                focusable="false"
-                                role="img"
-                                aria-label="Placeholder: 32x32"
-                            >
-                                <title>Placeholder</title>
-                                <rect fill="#007bff" width="100%" height="100%" />
-                                <text fill="#007bff" dy=".3em" x="50%" y="50%">
-                                    32x32
-                                </text>
-                            </svg>
-                            <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                                <div class="d-flex justify-content-between align-items-center w-100">
-                                    <strong class="text-gray-dark">Full Name</strong>
-                                    <a href="#">Follow</a>
-                                </div>
-                                <span class="d-block">@username</span>
-                            </div>
-                        </div>
-                        <div class="media text-muted pt-3">
-                            <svg
-                                class="bd-placeholder-img mr-2 rounded"
-                                width="32"
-                                height="32"
-                                xmlns="http://www.w3.org/2000/svg"
-                                preserveAspectRatio="xMidYMid slice"
-                                focusable="false"
-                                role="img"
-                                aria-label="Placeholder: 32x32"
-                            >
-                                <title>Placeholder</title>
-                                <rect fill="#007bff" width="100%" height="100%" />
-                                <text fill="#007bff" dy=".3em" x="50%" y="50%">
-                                    32x32
-                                </text>
-                            </svg>
-                            <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                                <div class="d-flex justify-content-between align-items-center w-100">
-                                    <strong class="text-gray-dark">Full Name</strong>
-                                    <a href="#">Follow</a>
-                                </div>
-                                <span class="d-block">@username</span>
-                            </div>
-                        </div>
-                        <div class="media text-muted pt-3">
-                            <svg
-                                class="bd-placeholder-img mr-2 rounded"
-                                width="32"
-                                height="32"
-                                xmlns="http://www.w3.org/2000/svg"
-                                preserveAspectRatio="xMidYMid slice"
-                                focusable="false"
-                                role="img"
-                                aria-label="Placeholder: 32x32"
-                            >
-                                <title>Placeholder</title>
-                                <rect fill="#007bff" width="100%" height="100%" />
-                                <text fill="#007bff" dy=".3em" x="50%" y="50%">
-                                    32x32
-                                </text>
-                            </svg>
-                            <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                                <div class="d-flex justify-content-between align-items-center w-100">
-                                    <strong class="text-gray-dark">Full Name</strong>
-                                    <a href="#">Follow</a>
-                                </div>
-                                <span class="d-block">@username</span>
-                            </div>
-                        </div>
-                        <small class="d-block text-right mt-3">
-                            <a href="#">All suggestions</a>
-                        </small>
-                    </div>
-                </main>
-            </div>
-        );
-    }
+  getGroupUsers() {
+    getGroupUsers().then(res => this.setState({ users: res.data.reverse() }));
+    console.log(this.state);
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <main role="main" class="container">
+          <div class="my-3 p-3 bg-white rounded shadow-sm">
+            <h6 class="border-bottom border-gray pb-2 mb-0">Пользователи</h6>
+                <UserView user/>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default UsersView;
--- src/components/ModelsView/ModelsView.jsx
+++ src/components/ModelsView/ModelsView.jsx
@@ -1,256 +1,302 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import $ from "jquery";
 
-import {getGroupModels, getUserModels, uploadModel} from "../../services/models";
+import {
+  getGroupModels,
+  getUserModels,
+  uploadModel
+} from "../../services/models";
 
 import HeaderComponent from "../HeaderComponent";
 import ModelItem from "../ModelItem/ModelItem";
 
 import "./ModelsView.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUpload} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 class ModelsView extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            models: [],
-            isDialogOpen: false,
-            title: "",
-            desc: "",
-            filename: "",
-            isUploaded: false
-        };
-
-        this.fileInput = React.createRef();
-
-        this.handleCloseDialog = this.handleCloseDialog.bind(this);
-        this.handleOpenDialog = this.handleOpenDialog.bind(this);
-        this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.handleDescChange = this.handleDescChange.bind(this);
-        this.handleUpload = this.handleUpload.bind(this);
-        this.handleFileSelection = this.handleFileSelection.bind(this);
-        this.handleModelRemoved = this.handleModelRemoved.bind(this);
-        this.getModels = this.getModels.bind(this);
-    }
-
-    componentDidMount = () => this.getModels();
-
-    getModels = () => {
-        if (this.props.groupModels)
-            getGroupModels(this.props.groupId).then(res => this.setState({models: res.data.reverse()}));
-        else
-            getUserModels().then(res => this.setState({models: res.data.reverse()}));
+    this.state = {
+      models: [],
+      isDialogOpen: false,
+      title: "",
+      desc: "",
+      filename: "",
+      isUploaded: false
     };
 
-    handleCloseDialog() {
-        this.setState({isDialogOpen: false});
+    this.fileInput = React.createRef();
+
+    this.handleCloseDialog = this.handleCloseDialog.bind(this);
+    this.handleOpenDialog = this.handleOpenDialog.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleDescChange = this.handleDescChange.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
+    this.handleFileSelection = this.handleFileSelection.bind(this);
+    this.handleModelRemoved = this.handleModelRemoved.bind(this);
+    this.getModels = this.getModels.bind(this);
+  }
+
+  componentDidMount = () => this.getModels();
+
+  /* Group & User interaction */
+  getModels = () => {
+    if (this.props.groupModels)
+      getGroupModels(this.props.groupId).then(res =>
+        this.setState({ models: res.data.reverse() })
+      );
+    else
+      getUserModels().then(res =>
+        this.setState({ models: res.data.reverse() })
+      );
+  };
+
+  isMarginAfterHeaderNecessary() {
+    return this.props.path == "/models"
+      ? "container margin-after-header"
+      : "container";
+  }
+
+  handleTablePreferences(modelCells) {
+    /* Getting the WHOLE table is because I'm gonna change the design a bit */
+    if (this.props.path == "/models") {
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Название</th>
+              <th scope="col" />
+              <th scope="col">Тип</th>
+              <th scope="col">Вес</th>
+              <th scope="col">Загружено</th>
+            </tr>
+          </thead>
+          <tbody>{modelCells}</tbody>
+        </table>
+      );
+    } else {
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Название</th>
+              <th scope="col">Пользователь</th>
+              <th scope="col">Тип</th>
+              <th scope="col">Вес</th>
+              <th scope="col">Загружено</th>
+            </tr>
+          </thead>
+          <tbody>{modelCells}</tbody>
+        </table>
+      );
     }
+  }
 
-    handleOpenDialog() {
-        this.setState({isDialogOpen: true});
-    }
+  /* Models interaction */
+  handleCloseDialog() {
+    this.setState({ isDialogOpen: false });
+  }
 
-    handleTitleChange(e) {
-        this.setState({title: e.target.value});
-    }
+  handleOpenDialog() {
+    this.setState({ isDialogOpen: true });
+  }
 
-    handleDescChange(e) {
-        this.setState({desc: e.target.value});
-    }
+  handleTitleChange(e) {
+    this.setState({ title: e.target.value });
+  }
 
-    handleFileSelection() {
-        console.log(this.fileInput.current.files);
-        if (this.fileInput.current.files)
-            this.setState({filename: this.fileInput.current.files[0].name});
-    }
+  handleDescChange(e) {
+    this.setState({ desc: e.target.value });
+  }
 
-    handleModelRemoved(data) {
-        let newModels = this.state.models.filter(model => {
-            return model.model_id !== data.id;
-        });
-        console.log(newModels);
-        this.setState(prev => ({
-            models: newModels
-        }));
-    }
+  handleFileSelection() {
+    console.log(this.fileInput.current.files);
+    if (this.fileInput.current.files)
+      this.setState({ filename: this.fileInput.current.files[0].name });
+  }
 
-    handleUpload() {
-        this.setState({isUploaded: true});
-        uploadModel(this.state.title, this.state.desc, this.fileInput.current.files[0], "NULL")
-            .then(() => {
-            this.setState({isUploaded: false, isDialogOpen: false});
-            $("#exampleModal").modal("hide");
-            this.getModels().then();
-        });
-    }
+  handleModelRemoved(data) {
+    let newModels = this.state.models.filter(model => {
+      return model.model_id !== data.id;
+    });
+    console.log(newModels);
+    this.setState(prev => ({
+      models: newModels
+    }));
+  }
 
-    render() {
-        const modelCells = this.state.models.map(model => {
-            return (
-                <ModelItem
-                    id={model.model_id}
-                    filename={model.filename}
-                    type={model.type}
-                    sizeKB={model.sizeKB}
-                    createdTime={model.createdTime}
-                    onModelRemoved={this.handleModelRemoved}
-                />
-            );
-        });
+  handleUpload() {
+    this.setState({ isUploaded: true });
+    uploadModel(
+      this.state.title,
+      this.state.desc,
+      this.fileInput.current.files[0],
+      "NULL"
+    ).then(() => {
+      this.setState({ isUploaded: false, isDialogOpen: false });
+      $("#exampleModal").modal("hide");
+      this.getModels().then();
+    });
+  }
 
-        return (
-            <div>
-                <div
-                    hidden={!this.state.isDialogOpen}
-                    className="modal fade"
-                    id="exampleModal"
-                    tabIndex="-1"
-                    role="dialog"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
+  render() {
+    const modelCells = this.state.models.map(model => {
+      return (
+        <ModelItem
+          id={model.model_id}
+          filename={model.filename}
+          type={model.type}
+          sizeKB={model.sizeKB}
+          createdTime={model.createdTime}
+          onModelRemoved={this.handleModelRemoved}
+        />
+      );
+    });
+
+    return (
+      <div>
+        <div
+          hidden={!this.state.isDialogOpen}
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Добавить модель
+                </h5>
+                <button
+                  onClick={this.handleCloseDialog}
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
                 >
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">
-                                    Добавить модель
-                                </h5>
-                                <button
-                                    onClick={this.handleCloseDialog}
-                                    type="button"
-                                    className="close"
-                                    data-dismiss="modal"
-                                    aria-label="Close"
-                                >
-                                    <span>&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="input-group" style={{marginBottom: "16px"}}>
-                                    <div className="input-group-prepend">
-                                        <span className="input-group-text" id="">Название</span>
-                                    </div>
-                                    <input
-                                        name="author"
-                                        type="text"
-                                        className="form-control"
-                                        onChange={this.handleTitleChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="form-group" style={{textAlign: "left"}}>
-                                    <label
-                                        style={{textAlign: "left"}}
-                                        htmlFor="exampleFormControlTextarea1"
-                                    >
-                                        Описание
-                                    </label>
-                                    <textarea
-                                        name="desc"
-                                        className="form-control"
-                                        id="exampleFormControlTextarea1"
-                                        rows="5"
-                                        onChange={this.handleDescChange}
-                                        required
-                                    />
-                                </div>
-                                <div className="custom-file">
-                                    <div className="input-group">
-                                        <div className="custom-file">
-                                            <input
-                                                ref={this.fileInput}
-                                                className="custom-file-input"
-                                                name="model"
-                                                id="inputGroupFile04"
-                                                type="file"
-                                                required=""
-                                            />
-                                            <label
-                                                style={{textAlign: "left"}}
-                                                className="custom-file-label"
-                                                htmlFor="inputGroupFile04"
-                                            >
-                                                Выберете файл
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button
-                                    onClick={this.handleCloseDialog}
-                                    type="button"
-                                    className="btn btn-secondary"
-                                    data-dismiss="modal"
-                                >
-                                    Отмена
-                                </button>
-                                {this.state.isUploaded ? (
-                                    <button className="btn btn-primary" type="button" disabled>
-                                      <span
-                                          style={{marginRight: "8px"}}
-                                          className="spinner-border spinner-border-sm"
-                                          role="status"
-                                          aria-hidden="true"
-                                      /> Загрузка...
-                                    </button>
-                                ) : (
-                                    <button
-                                        type="button"
-                                        className="btn btn-primary"
-                                        onClick={this.handleUpload}
-                                    >
-                                        Отправить
-                                    </button>
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                  <span>&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="input-group" style={{ marginBottom: "16px" }}>
+                  <div className="input-group-prepend">
+                    <span className="input-group-text" id="">
+                      Название
+                    </span>
+                  </div>
+                  <input
+                    name="author"
+                    type="text"
+                    className="form-control"
+                    onChange={this.handleTitleChange}
+                    required
+                  />
                 </div>
+                <div className="form-group" style={{ textAlign: "left" }}>
+                  <label
+                    style={{ textAlign: "left" }}
+                    htmlFor="exampleFormControlTextarea1"
+                  >
+                    Описание
+                  </label>
+                  <textarea
+                    name="desc"
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="5"
+                    onChange={this.handleDescChange}
+                    required
+                  />
+                </div>
+                <div className="custom-file">
+                  <div className="input-group">
+                    <div className="custom-file">
+                      <input
+                        ref={this.fileInput}
+                        className="custom-file-input"
+                        name="model"
+                        id="inputGroupFile04"
+                        type="file"
+                        required=""
+                      />
+                      <label
+                        style={{ textAlign: "left" }}
+                        className="custom-file-label"
+                        htmlFor="inputGroupFile04"
+                      >
+                        Выберете файл
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  onClick={this.handleCloseDialog}
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Отмена
+                </button>
+                {this.state.isUploaded ? (
+                  <button className="btn btn-primary" type="button" disabled>
+                    <span
+                      style={{ marginRight: "8px" }}
+                      className="spinner-border spinner-border-sm"
+                      role="status"
+                      aria-hidden="true"
+                    />{" "}
+                    Загрузка...
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={this.handleUpload}
+                  >
+                    Отправить
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
 
-                <HeaderComponent />
+        <HeaderComponent />
 
-                <main role="main" className="container margin-after-header">
-                    <div className="my-3 p-3 bg-white rounded shadow-sm">
-                        <h3 className="inline">Модели</h3>
+        <main role="main" className={this.isMarginAfterHeaderNecessary()}>
+          <div className="my-3 p-3 bg-white rounded shadow-sm">
+            <h3 className="inline">Модели</h3>
 
-                        <div className="inline">
-                            <FontAwesomeIcon
-                                className="tool"
-                                onClick={this.handleOpenDialog}
-                                transform="grow-7 left-1.5 up-2.2"
-                                data-toggle="modal"
-                                data-target="#exampleModal"
-                                icon={faUpload}
-                            />
-                        </div>
+            <div className="inline">
+              <FontAwesomeIcon
+                className="tool"
+                onClick={this.handleOpenDialog}
+                transform="grow-7 left-1.5 up-2.2"
+                data-toggle="modal"
+                data-target="#exampleModal"
+                icon={faUpload}
+              />
+            </div>
 
-                        <h3 className="border-bottom border-gray pb-2 mb-0" />
-                        {/* <button class="btn btn-primary" onClick={this.handleOpenDialog} data-toggle="modal" data-target="#exampleModal">
+            <h3 className="border-bottom border-gray pb-2 mb-0" />
+            {/* <button class="btn btn-primary" onClick={this.handleOpenDialog} data-toggle="modal" data-target="#exampleModal">
                 <i class="icon-excel"></i>Добавить
               </button> */}
 
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th scope="col">Название</th>
-                                <th scope="col" />
-                                <th scope="col">Тип</th>
-                                <th scope="col">Вес</th>
-                                <th scope="col">Загружено</th>
-                            </tr>
-                            </thead>
-                            <tbody>{modelCells}</tbody>
-                        </table>
-                    </div>
-                </main>
-            </div>
-
-        );
-    }
+            {this.handleTablePreferences(modelCells)}
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default ModelsView;
