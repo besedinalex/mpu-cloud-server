631d29701a6b828f03578b376e80cd678f891ca7
Alexey <besedinalexey@gmail.com>
2019-10-18 16:44:22 +0300

    Basic API

--- server/embedded-viewer/src/main.js
+++ server/embedded-viewer/src/main.js
@@ -2,36 +2,41 @@ import axios from 'axios';
 
 import * as viewer from './viewer';
 
-const serverURL = 'http://127.0.0.1:4000';
-
-const viewerDiv = document.getElementById('mpu-cloud-viewer');
-
-function loadExternalStyle(path) {
-    const style = document.createElement("link");
-    style.rel = 'stylesheet';
-    style.type = 'text/css';
-    style.href = path;
-    style.setAttribute('mpu-cloud', 'viewer');
-    document.getElementsByTagName("head")[0].appendChild(style);
-}
-
-function loadExternalStyles() {
-    const styles = ['styles.css', 'bootstrap.min.css', 'all.min.css', 'annotation-style.css'];
-    for (const style of styles) {
-        loadExternalStyle(`${serverURL}/viewer/styles/${style}`);
-    }
-}
-
-function unloadExternalStyles() {
-    const loadedStyles = document.querySelectorAll('[mpu-cloud="viewer"]');
-    for (const style of loadedStyles) {
-        style.remove();
-    }
-}
-
 window.onload = () => {
-    loadExternalStyles();
-    axios.get(`${serverURL}/viewer/viewer.html`)
-        .then(res => viewerDiv.innerHTML = res.data);
-    viewer.init();
+    window.MPUCloudViewer = MPUCloudViewer;
+};
+
+const MPUCloudViewer = {
+    serverURL: 'http://127.0.0.1:4000',
+    viewerDiv: document.getElementById('mpu-cloud-viewer'),
+
+    init: function() {
+        this.loadExternalStyles();
+        axios.get(`${this.serverURL}/viewer/viewer.html`)
+            .then(res => this.viewerDiv.innerHTML = res.data);
+        viewer.init();
+    },
+
+    loadExternalStyle: function(path) {
+        const style = document.createElement("link");
+        style.rel = 'stylesheet';
+        style.type = 'text/css';
+        style.href = path;
+        style.setAttribute('mpu-cloud', 'viewer');
+        document.getElementsByTagName("head")[0].appendChild(style);
+    },
+
+    loadExternalStyles: function() {
+        const styles = ['styles.css', 'bootstrap.min.css', 'all.min.css', 'annotation-style.css'];
+        for (const style of styles) {
+            this.loadExternalStyle(`${this.serverURL}/viewer/styles/${style}`);
+        }
+    },
+
+    unloadExternalStyles: function() {
+        const loadedStyles = document.querySelectorAll('[mpu-cloud="viewer"]');
+        for (const style of loadedStyles) {
+            style.remove();
+        }
+    }
 };
