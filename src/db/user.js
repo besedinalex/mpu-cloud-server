decabd499ff4b02982e8c91ccb5ecc2a7a555462
Sevka <sevkaplay@gmail.com>
2020-04-13 21:36:33 +0300

    Add universal method for finding user + description

--- src/db/user.js
+++ src/db/user.js
@@ -1,18 +1,17 @@
-const db = require('./db-connection');
+const db = require("./db-connection");
 
 exports.signUp = function (firstName, lastName, email, password) {
     return new Promise((resolve, reject) => {
-        const sql =
-        `INSERT INTO Users (firstName, lastName, email, password)
+        const sql = `INSERT INTO Users (firstName, lastName, email, password)
         VALUES ('${firstName}','${lastName}','${email}','${password}')`;
-        db.run(sql, [], function(err) {
+        db.run(sql, [], function (err) {
             if (err) {
                 reject(err);
             } else {
                 resolve(this.lastID);
             }
-        })
-    })
+        });
+    });
 };
 
 exports.signIn = function (email) {
@@ -27,49 +26,48 @@ exports.signIn = function (email) {
                 resolve(rows[0]);
             }
         });
-    })
+    });
 };
 
 exports.getUser = function (user_id) {
     return new Promise((resolve, reject) => {
-        const sql =
-        `SELECT Users.user_id,  Users.createdTime, Users.firstName, Users.lastName, Users.email
+        const sql = `SELECT Users.user_id,  Users.createdTime, Users.firstName, Users.lastName, Users.email
         FROM Users
         WHERE Users.user_id ='${user_id}'`;
-        db.all(sql, [], function(err, rows) {
+        db.all(sql, [], function (err, rows) {
             if (err) {
                 reject(err);
             } else {
                 resolve(rows);
             }
-        })
-    })
+        });
+    });
 };
 
 exports.getIdByEmail = function (email) {
     return new Promise((resolve, reject) => {
         const sql = `SELECT Users.user_id FROM Users WHERE Users.email = '${email}'`;
-        db.all(sql, [], function(err, rows) {
+        db.all(sql, [], function (err, rows) {
             if (err) {
                 reject(err);
             } else {
                 resolve(rows);
             }
-        })
-    })
+        });
+    });
 };
 
 exports.getEmailById = function (id) {
     return new Promise((resolve, reject) => {
         const sql = `SELECT Users.email FROM Users WHERE Users.user_id = '${id}'`;
-        db.all(sql, [], function(err, rows) {
+        db.all(sql, [], function (err, rows) {
             if (err) {
                 reject(err);
             } else {
                 resolve(rows);
             }
-        })
-    })
+        });
+    });
 };
 
 exports.getUserFiles = function (userId) {
@@ -82,18 +80,57 @@ exports.getUserFiles = function (userId) {
                 resolve(rows);
             }
         });
-    })
+    });
 };
 
-exports.insertResetToken = function (userId, resetToken, resetTokenExp){
+exports.insertResetToken = function (userId, resetToken, resetTokenExp) {
     return new Promise((resolve, reject) => {
-        const sql = `UPDATE Users SET resetToken="${resetToken}", resetTokenExp="${resetTokenExp}" WHERE user_id=${userId}`
-        db.run(sql, [], function(err) {
+        const sql = `UPDATE Users SET resetToken="${resetToken}", resetTokenExp="${resetTokenExp}" WHERE user_id=${userId}`;
+        db.run(sql, [], function (err) {
             if (err) {
                 reject(err);
             } else {
                 resolve(this.lastID);
             }
-        })
-    })
-}
+        });
+    });
+};
+
+/* Функция поиска в таблице Users, по заданным параметрам.
+Принимает: объект, содержащий поля: название столбца в таблице БД, как КЛЮЧ поля и значения для поиска в этом поле
+
+{ field1: <value>, field2: <value> ... } 
+
+Возвращает массив пользователей, которые соотвествуют параметрам
+
+    userData.find({
+        firstName: "Всеволод",
+        lastName: "Кочнев"
+    });
+    
+Найдет всех пользователей с именем "Всеволод" и фамилией "Кочнев"
+*/
+
+exports.find = function (params) {
+    const paramsKeys = Object.keys(params);
+    return new Promise((resolve, reject) => {
+        try {
+            let sql = `SELECT * FROM Users WHERE`;
+            paramsKeys.forEach((value, i) => {
+                if (i != 0) {
+                    sql += ` AND`;
+                }
+                sql += ` ${value} = "${params[value]}"`;
+            });
+            db.all(sql, [], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+            });
+        } catch (error) {
+            throw error;
+        }
+    });
+};
