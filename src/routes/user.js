fba199b229a575db9e96b088a25e66b00fbea256
Sevka <sevkaplay@gmail.com>
2020-04-23 23:02:40 +0300

    Add one-way hash encryption

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,8 +9,9 @@ const resetEmail = require('../../emails/reset')
 const nodemailer = require("nodemailer");
 const sendgrid = require("nodemailer-sendgrid-transport");
 const keys = require("../../config.json");
-const {validationResult} = require('express-validator')
-const {registerValidators} = require('../utils/validators')
+const {validationResult} = require('express-validator');
+const {registerValidators} = require('../utils/validators');
+const bcrypt = require("bcryptjs");
 
 let lastPasswordResetId = 0;
 const resetRequests = [];
@@ -45,8 +46,12 @@ user.get("/token", function (req, res) {
     const email = req.query.email.toLowerCase();
     const {password} = req.query;
     userData.signIn(email, password)
-        .then((data) => {
-            if (decrypt(data.password) !== password) {
+        .then(async (data) => {
+            const areSame = await bcrypt.compare(
+                password,
+                data.password
+            );
+            if (!areSame) {
                 res.status(401).send({message: 'Неверный email или пароль.'});
             } else {
                 const payload = {
@@ -69,13 +74,17 @@ user.get("/data", function (req, res) {
         .catch(() => res.status(404).send({message: 'Такой пользователь не найден.'}));
 });
 
-user.post("/data", registerValidators, function (req, res) {
+user.post("/data", registerValidators, async function (req, res) {
     const {firstName, lastName, email, password} = req.query;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(422).send({errors: errors.array()});
     } else {
-        userData.signUp(firstName, lastName, email, encrypt(password))
+        //Шифратор паролей, второй параметр - точность шифратора - чем больше, тем круче шифрование
+        //но дольше времени занимает
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        userData.signUp(firstName, lastName, email, hashPassword)
             .then(userId => {
                 const payload = {
                     id: userId,
