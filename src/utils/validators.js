925a4210bbdc7a2ba896205bf6ed6bc8c9e67424
Alexey Besedin <besedinalexey@gmail.com>
2020-08-08 19:09:32 +0300

    Password reset via CLI & updated validators

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,12 +1,10 @@
 const {query} = require("express-validator");
+const {PASSWORD_LENGTH_MIN, PASSWORD_LENGTH_MAX} = require(process.cwd() + '/config.json');
 
 exports.registerValidators = [
     query("email")
         .isEmail()
-        .withMessage("Email не соответствует правилам сервера."),
-    query("password")
-        .isLength({ min: 6, max: 56 })
-        .withMessage("Пароль не соответствует правилам сервера.")
+        .withMessage("Email не соответствует правилам сервера.")
         .trim(),
     query("lastName")
         .custom((value, {}) => {
@@ -23,5 +21,12 @@ exports.registerValidators = [
             }
             return true;
         })
+        .trim()
+];
+
+exports.passwordValidator = [
+    query("password")
+        .isLength({min: PASSWORD_LENGTH_MIN, max: PASSWORD_LENGTH_MAX})
+        .withMessage(`Пароль должен содержать от ${PASSWORD_LENGTH_MIN} до ${PASSWORD_LENGTH_MAX} символов.`)
         .trim()
 ];
